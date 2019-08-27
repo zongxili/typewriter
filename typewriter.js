cd85dbc6bb25c37972e3a699f59b1b3de40303b1
Zongxi Li <zongxiliwork@gmail.com>
2019-08-27 20:27:26 +0000

    first commit

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,13 @@
 const sentence = "hello there from lighthouse labs";
+let sec = 0;
+// for (const char of sentence) {
+for (let i = 0; i < sentence.length; i++) {
+  let char = sentence.charAt(i);
+  sec += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+    if (i === (sentence.length - 1)) {
+      console.log();
+    }
+  }, sec); // <= 1s delay to make it noticeable. Can dial it down later.
+}
